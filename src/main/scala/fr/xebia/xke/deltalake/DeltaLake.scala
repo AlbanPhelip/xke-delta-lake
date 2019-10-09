fb1f34e548c89396a744e7ffe9ac82988418fc36
AlbanPhelip <alban.phelip@gmail.com>
2019-10-09 16:51:18 +0200

    Upsert test

--- src/main/scala/fr/xebia/xke/deltalake/DeltaLake.scala
+++ src/main/scala/fr/xebia/xke/deltalake/DeltaLake.scala
@@ -1,12 +1,51 @@
 package fr.xebia.xke.deltalake
 
-import fr.xebia.xke.deltalake.utils.SparkSessionProvider
+import fr.xebia.xke.deltalake.model.Person
 import fr.xebia.xke.deltalake.utils.ExtensionMethodsUtils._
-import org.apache.spark.sql.{DataFrame, SaveMode}
+import fr.xebia.xke.deltalake.utils.SparkSessionProvider
 import io.delta.tables._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Dataset, Row, SaveMode}
 
 object DeltaLake extends App with SparkSessionProvider {
 
   import spark.implicits._
+
+  val rootPath = args.head
+  val personPath = s"$rootPath/person"
+
+  val persons = List(
+    Person("Toto", 21, "2019-10-01"),
+    Person("Titi", 30, "2019-10-01")
+  ).toDF()
+
+  persons.write.mode(SaveMode.Overwrite).delta(personPath)
+
+  val newPerson = List(
+    Person("Toto", 22, "2019-10-09"),
+    Person("Tata", 51, "2019-10-09")
+  ).toDF()
+
+  val deltaPerson = DeltaTable.forPath(personPath)
+  val deltaDf: Dataset[Row] = deltaPerson.toDF
+
+  deltaDf.show()
+
+  val columnsUpdate = deltaDf
+    .columns
+    .map(column => column -> col(s"newPerson.$column"))
+    .toMap
+
+  deltaPerson.as("oldPerson")
+    .merge(
+      newPerson.as("newPerson"),
+      "oldPerson.name = newPerson.name"
+    ).whenMatched()
+    .update(columnsUpdate)
+    .whenNotMatched
+    .insert(columnsUpdate)
+    .execute()
+
+  deltaDf.show()
 
 }
---
+++ src/main/scala/fr/xebia/xke/deltalake/model/Person.scala
@@ -0,0 +1,5 @@
+package fr.xebia.xke.deltalake.model
+
+case class Person(name: String,
+                  age: Int,
+                  date: String)
