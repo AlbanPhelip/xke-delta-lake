504916f3f2e85b25f0c462bf81ddbe5e87cdbb06
Alban Phélip <alban.phelip-ext@sanofi.com>
2019-10-09 16:23:24 +0200

    Move benchmark code

--- src/main/scala/fr/xebia/xke/deltalake/DeltaLake.scala
+++ src/main/scala/fr/xebia/xke/deltalake/DeltaLake.scala
@@ -7,40 +7,4 @@ import io.delta.tables._
 
 object DeltaLake extends App with SparkSessionProvider {
 
-  import spark.implicits._
-
-  val df: DataFrame =  (1 to 100000).toDF("col1").coalesce(1).cache()
-  df.count()
-
-  val rootPath = args.head
-  val delta = s"$rootPath/table_delta"
-  val parquet = s"$rootPath/table_parquet"
-  val saveMode = SaveMode.Append
-
-
-  val t3: Long = System.currentTimeMillis()
-  df.write.mode(saveMode).parquet(parquet)
-  val t4: Long = System.currentTimeMillis()
-
-  val t1: Long = System.currentTimeMillis()
-  df.write.mode(saveMode).delta(delta)
-  val t2: Long = System.currentTimeMillis()
-
-  val t5: Long = System.currentTimeMillis()
-  spark.read.delta(delta).count
-  val t6: Long = System.currentTimeMillis()
-
-  val t7: Long = System.currentTimeMillis()
-  spark.read.parquet(parquet).count
-  val t8: Long = System.currentTimeMillis()
-
-  println(s"Time writing delta: ${t2-t1} ms")
-  println(s"Time writing parquet: ${t4-t3} ms")
-  println(s"Time reading delta: ${t6-t5} ms")
-  println(s"Time reading parquet: ${t8-t7} ms")
-
-  val deltaTable: DeltaTable = DeltaTable.forPath(spark, delta)
-
-  deltaTable.history(10).show(truncate = false)
-
 }
